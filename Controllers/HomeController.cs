ec413a2054262480e88777d6fe92e7833477a448
AMSAGUINE Oualid <oualidams13@gmail.com>
2024-03-25 12:10:06 +0000

    oualid changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace E_Commerce.Controllers
 {
     public class HomeController : Controller
     {
+        //My first change.
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
