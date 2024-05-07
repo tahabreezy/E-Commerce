10108fedf3a1607db3774f3f63a274c99a760587
Oussama Hasni <Ussama.hasni001@gmail.com>
2024-05-07 14:25:22 +0100

    commit72

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,9 +40,12 @@ namespace E_Commerce.Controllers
         public int somme (int a,int b)
         {
 
-            int s = a / 0;
+      
 
-            return s + b;
+
+
+
+            return a + b;
         }
     }
 }
