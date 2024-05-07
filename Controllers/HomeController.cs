28ddd2ad57c64dd8d7a9c62bb7bb2c4150818989
GRAMI SALMA <salmagrami0@gmail.com>
2024-05-07 14:33:21 +0100

    Code corrige Grami Salma

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,10 +39,10 @@ namespace E_Commerce.Controllers
         /// le code corrige 
         public int somme (int a,int b)
         {
+            
+            
 
-            int s = a / 0;
-
-            return s + b;
+            return a + b;
         }
     }
 }
