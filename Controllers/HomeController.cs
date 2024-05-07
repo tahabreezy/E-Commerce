3b974c37ef0447c0e4c269208b90820d9ef96266
Wissal <wissalchakir7@gmail.com>
2024-05-07 14:17:57 +0100

    commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,7 +40,7 @@ namespace E_Commerce.Controllers
         public int somme (int a,int b)
         {
 
-            int s = a / 0;
+            int s = a / b;
 
             return s + b;
         }
